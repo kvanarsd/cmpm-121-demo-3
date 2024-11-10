e20ce89c57125f5a67507af677016f5e7ecea2ff
kvanarsd <kvanarsd@ucsc.edu>
2024-11-09 19:01:40 -0800

    Restructure and add serial

--- src/main.ts
+++ src/main.ts
@@ -13,17 +13,25 @@ import luck from "./luck.ts";
 const APP_NAME = "GeoCoin";
 document.title = APP_NAME;
 
-let playerCoins = 0;
+const playerCoins = Array<Coin>();
 const status = document.querySelector<HTMLDivElement>("#statusPanel")!;
-status.innerHTML = `You have ${playerCoins} coin(s)`;
+status.innerHTML = `You have ${playerCoins.length} coin(s)`;
 
 // map variables -------------------------------------------------------
+interface Coin {
+  serial: string;
+}
+interface Cache {
+  coordinates: Array<number>;
+  coins: Array<Coin>;
+}
+
 const playerLocation = [36.989498, -122.062777];
 const zoomAmount = 19;
 const tileSize = 1e-4;
 const neighborhoodSize = 8;
 const cacheChance = 0.1;
-const coinCache = new Map<string, number>();
+const coinCache = new Map<string, Cache>();
 
 // create map
 const map = leaflet.map("map", {
@@ -43,17 +51,34 @@ const playerMarker = leaflet.marker(playerLocation).addTo(map);
 playerMarker.bindTooltip("This is you!");
 
 // functions -------------------------------------------------------
+function getCell(lat: number, lon: number): Cache {
+  const key = getKey(lat, lon);
 
-// update cache when player collects or deposits coins
-function updateCache(change: number, position: string, popup: HTMLDivElement) {
-  let currentAmount = coinCache.get(position) || 0;
-  if ((change > 0 && currentAmount > 0) || (change < 0 && playerCoins > 0)) {
-    currentAmount -= change;
-    playerCoins += change;
-    coinCache.set(position, currentAmount);
-    popup.querySelector<HTMLSpanElement>("#coin")!.innerHTML =
-      `${currentAmount}`;
-    status.innerHTML = `You have ${playerCoins} coin(s)`;
+  let cache = coinCache.get(key);
+  if (cache == undefined) {
+    const coins = Array<Coin>();
+    for (let i = 0; i < Math.floor(luck([lat, lon].toString()) * 100); i++) {
+      coins.push({ serial: `${key}#${i}` });
+    }
+    cache = { coordinates: [lat, lon], coins: coins };
+    coinCache.set(key, cache);
+  }
+
+  return cache;
+}
+
+function getKey(lat: number, lon: number) {
+  const i = Math.floor(lat * 100000);
+  const j = Math.floor(lon * 100000);
+  return `${i}:${j}`;
+}
+
+function updateCache(add: Array<Coin>, remove: Array<Coin>) {
+  if (remove.length > 0) {
+    const coin = remove.pop()!;
+    console.log(coin.serial);
+    add.push(coin);
+    status.innerHTML = `You have ${playerCoins.length} coin(s)`;
   }
 }
 
@@ -66,32 +91,34 @@ function placeCache(y: number, x: number) {
 
   const rect = leaflet.rectangle(bounds);
   rect.addTo(map);
-  const positionKey = `${y},${x}`;
 
   // cache popup
   rect.bindPopup(() => {
-    // store coin amount
-    if (!coinCache.has(positionKey)) {
-      coinCache.set(positionKey, Math.floor(luck([y, x].toString()) * 100));
-    }
-
-    const coinAmount = coinCache.get(positionKey)!;
+    const coinAmount = getCell(y, x)!.coins;
 
     const popup = document.createElement("div");
     popup.innerHTML = `
-          <div>There are <span id="coin">${coinAmount}</span> coin(s) here!</div>
+          <div>There are <span id="coin">${coinAmount.length}</span> coin(s) here!</div>
           <button id="collect">Collect</button>
           <button id="deposit">Deposit</button>
     `;
     popup.querySelector<HTMLButtonElement>("#collect")!
       .addEventListener(
         "click",
-        () => updateCache(1, positionKey, popup),
+        () => {
+          updateCache(playerCoins, coinAmount);
+          popup.querySelector<HTMLSpanElement>("#coin")!.innerHTML =
+            `${coinAmount.length}`;
+        },
       );
     popup.querySelector<HTMLButtonElement>("#deposit")!
       .addEventListener(
         "click",
-        () => updateCache(-1, positionKey, popup),
+        () => {
+          updateCache(coinAmount, playerCoins);
+          popup.querySelector<HTMLSpanElement>("#coin")!.innerHTML =
+            `${coinAmount.length}`;
+        },
       );
 
     return popup;
